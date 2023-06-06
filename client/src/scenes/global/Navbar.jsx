23f65e25f892468e48e7e142b88395fbc619f1ef
fkaaziebu <frederickaziebu1998@gmail.com>
2023-06-06 00:42:20 +0000

    Backend re-deploy

--- client/src/scenes/global/Navbar.jsx
+++ client/src/scenes/global/Navbar.jsx
@@ -13,17 +13,19 @@ function Navbar() {
         <Link className="navbar-brand fw-bold fs-1" to="/">
           VPASS
         </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
+        {user?.token && (
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+        )}
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
             {user.username && (
--- server/index.js
+++ server/index.js
@@ -2,5 +2,6 @@ const app = require("./src/app");
 const sequelize = require("./src/config/database");
 
 sequelize.sync({ force: true });
+// sequelize.sync({ force: true });
 
 app.listen(3001, () => console.log("App is running!"));
